7569a1884a2a2766c0313023b37087360c77ff66
spectating101 <s1133958@mail.yzu.edu.tw>
2026-07-12 15:41:15 +0800

    design: load premium component treatments

--- drive/src/v2/main.jsx
+++ drive/src/v2/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./v2-base.css";
 import "./v2.css";
 import "./premium.css";
+import "./premium-components.css";
 import { V2App } from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(<V2App />);
