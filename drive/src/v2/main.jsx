fa54010da54c43c9168819d5caefada1068e70df
spectating101 <s1133958@mail.yzu.edu.tw>
2026-07-12 15:47:54 +0800

    design: load signature profile composition

--- drive/src/v2/main.jsx
+++ drive/src/v2/main.jsx
@@ -3,6 +3,7 @@ import "./v2-base.css";
 import "./v2.css";
 import "./premium.css";
 import "./premium-components.css";
+import "./premium-profile.css";
 import { V2App } from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(<V2App />);
