0147e12ee878ddfadafd51241fb6930ea35ddb64
spectating101 <s1133958@mail.yzu.edu.tw>
2026-07-12 15:38:17 +0800

    design: load premium visual authority layer

--- drive/src/v2/main.jsx
+++ drive/src/v2/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./v2-base.css";
 import "./v2.css";
+import "./premium.css";
 import { V2App } from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(<V2App />);
