eb3aefc41389cf979b4c3ddcda7e237e5570f0fc
spectating101 <s1133958@mail.yzu.edu.tw>
2026-07-23 03:10:48 +0800

    activate convergence link fix

--- drive/src/v2/main.jsx
+++ drive/src/v2/main.jsx
@@ -16,6 +16,7 @@ import "./rc3-semantic.css";
 import "./rc3-visual-fixes.css";
 import "./rc3-recovery-links.css";
 import "./live-convergence.css";
+import "./live-convergence-fixes.css";
 import { InteractionProvider } from "./InteractionGuidance.jsx";
 import { V2App } from "./App.jsx";
 
