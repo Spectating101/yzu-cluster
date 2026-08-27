c05e342654d29f8793eceaf608b07d735beabf57
spectating101 <s1133958@mail.yzu.edu.tw>
2026-08-27 18:28:01 +0800

    fix: load verified HPS responsive corrections

--- drive/src/v2/main.jsx
+++ drive/src/v2/main.jsx
@@ -15,6 +15,7 @@ import "./decoration-layer.css";
 import "./release-scale.css"; /* laptop/desktop resolution ladder + rail collapse */
 import "./library-workspace.css";
 import "./hps-shell-convergence.css";
+import "./hps-responsive-fixes.css";
 import { InteractionProvider } from "./InteractionGuidance.jsx";
 import { V2App } from "./App.jsx";
 
