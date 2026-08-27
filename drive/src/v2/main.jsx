3d2a914980ae8671800fb8b0fa84f60018edf95c
spectating101 <s1133958@mail.yzu.edu.tw>
2026-08-27 18:00:15 +0800

    feat(ui): load HPS shell convergence layer

--- drive/src/v2/main.jsx
+++ drive/src/v2/main.jsx
@@ -14,6 +14,7 @@ import "./interaction-feedback.css";
 import "./decoration-layer.css";
 import "./release-scale.css"; /* laptop/desktop resolution ladder + rail collapse */
 import "./library-workspace.css";
+import "./hps-shell-convergence.css";
 import { InteractionProvider } from "./InteractionGuidance.jsx";
 import { V2App } from "./App.jsx";
 
