f5aa3f2d1dc9481538135fc2c8ea022089cf3c4b
spectating101 <s1133958@mail.yzu.edu.tw>
2026-08-31 01:35:50 +0800

    Load visual hardening layer last

--- drive/src/v2/main.jsx
+++ drive/src/v2/main.jsx
@@ -26,6 +26,7 @@ import "./hps-responsive-fixes.css";
 import "./synthesis-workstation.css";
 import "./discover-frozen-closure.css";
 import "./connected-accounts.css";
+import "./visual-hardening.css";
 import { V2App } from "./App";
 import { InteractionProvider } from "./InteractionGuidance";
 
