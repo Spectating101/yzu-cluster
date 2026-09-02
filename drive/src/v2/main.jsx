7aa7967c9b5768bc456d64b34e5795aff253bf51
spectating101 <s1133958@mail.yzu.edu.tw>
2026-09-02 19:08:37 +0800

    polish(synthesis): load workstation depth authority

--- drive/src/v2/main.jsx
+++ drive/src/v2/main.jsx
@@ -40,6 +40,7 @@ import "./synthesis-zero-instruction-visual-final.css";
 import "./synthesis-horizontal-workbench.css";
 import "./synthesis-horizontal-workbench-closure.css";
 import "./synthesis-horizontal-workbench-sanding.css";
+import "./synthesis-workbench-depth.css";
 import "./discover-frozen-closure.css";
 import "./connected-accounts.css";
 import { V2App } from "./App";
