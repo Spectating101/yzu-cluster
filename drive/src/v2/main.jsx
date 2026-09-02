8a3ba576efa36240e55433ca6ffec15297451d45
spectating101 <s1133958@mail.yzu.edu.tw>
2026-09-02 15:26:28 +0800

    polish(synthesis): load wide research brief ledger

--- drive/src/v2/main.jsx
+++ drive/src/v2/main.jsx
@@ -29,6 +29,7 @@ import "./synthesis-wide-workstation-closure.css";
 import "./synthesis-wide-workstation-sanding.css";
 import "./synthesis-wide-workstation-sanding-final.css";
 import "./synthesis-wide-shell-canvas.css";
+import "./synthesis-wide-brief-ledger.css";
 import "./discover-frozen-closure.css";
 import "./connected-accounts.css";
 import { V2App } from "./App";
