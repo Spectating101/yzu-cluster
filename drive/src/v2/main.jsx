066820f22ac192f07a8093bdae6b1c7d66a5071f
spectating101 <s1133958@mail.yzu.edu.tw>
2026-09-02 16:40:59 +0800

    reopen(synthesis): load operated-flow authority last

--- drive/src/v2/main.jsx
+++ drive/src/v2/main.jsx
@@ -31,6 +31,7 @@ import "./synthesis-wide-workstation-sanding-final.css";
 import "./synthesis-wide-shell-canvas.css";
 import "./synthesis-wide-brief-ledger.css";
 import "./synthesis-operation-truth.css";
+import "./synthesis-operated-flow.css";
 import "./discover-frozen-closure.css";
 import "./connected-accounts.css";
 import { V2App } from "./App";
