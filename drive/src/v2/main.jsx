5ac771991c92bb9ad341d2a26e4f934fd2fa6a45
spectating101 <s1133958@mail.yzu.edu.tw>
2026-09-03 21:13:55 +0800

    style(discover): load idle capability rail last

--- drive/src/v2/main.jsx
+++ drive/src/v2/main.jsx
@@ -32,6 +32,7 @@ import "./discover-canonical-polish.css";
 import "./discover-idle-visual-closure.css";
 import "./discover-idle-engine-showcase.css";
 import "./discover-idle-engine-refine.css";
+import "./discover-idle-capability-rail.css";
 import { V2App } from "./App";
 import { InteractionProvider } from "./InteractionGuidance";
 
