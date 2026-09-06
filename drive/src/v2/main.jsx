a19ca350de1cf318376b190eb496ab1ae4497655
spectating101 <s1133958@mail.yzu.edu.tw>
2026-09-07 00:18:37 +0800

    Load Home scroll closure last

--- drive/src/v2/main.jsx
+++ drive/src/v2/main.jsx
@@ -52,6 +52,7 @@ import "./synthesis-agent-console.css";
 import "./synthesis-object-context.css";
 import "./discover-frozen-closure.css";
 import "./connected-accounts.css";
+import "./home-release-closure.css";
 import { V2App } from "./App";
 import { InteractionProvider } from "./InteractionGuidance";
 import { SynthesisAuthorityMount } from "./SynthesisAuthorityMount.jsx";
